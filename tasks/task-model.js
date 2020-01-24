2664c7fc799ba5336333be8daf4fdc9755e12eb0
toddmurphy <toddmurphy93@gmail.com>
2020-01-24 14:04:57 -0400

    can create a new task and get all tasks - now just to try and join them

--- tasks/task-model.js
+++ tasks/task-model.js
@@ -11,6 +11,9 @@ function getTasks() {
   return db('tasks');
 }
 
+//retrieving a list of tasks.
+//The list of tasks should include the project name and project description.
+
 //add single task
 function addTask(task) {
   return db('tasks').insert(task);
