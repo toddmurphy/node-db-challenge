cd90dddf8e16ff24e1547637a255abdde623274a
toddmurphy <toddmurphy93@gmail.com>
2020-01-24 13:13:46 -0400

    i think migrations is completed and as per the readme

--- data/migrations/20200124124242_projects.js
+++ data/migrations/20200124124242_projects.js
@@ -2,16 +2,65 @@ exports.up = function(knex) {
   //resources table
   return (
     knex.schema
-      .createTable('resources', tbl => {})
+      .createTable('resources', tbl => {
+        tbl.increments();
 
+        tbl
+          .string('name', 255)
+          .unique()
+          .notNullable(); //no duplicate names
+        tbl.string('description', 255).notNullable();
+      })
       //projects table
-      .createTable('projects', tbl => {})
+      .createTable('projects', tbl => {
+        tbl.increments();
 
+        tbl.string('name', 255).notNullable();
+        tbl.string('description', 255).notNullable();
+        tbl.boolean('completed').defaultTo(false);
+      })
       //tasks table
-      .createTable('tasks', tbl => {})
-  );
+      .createTable('tasks', tbl => {
+        tbl.increments();
 
-  //connector table --> foreign keys
+        tbl.string('description', 255).notNullable();
+        tbl.string('notes', 300).notNullable();
+        tbl.boolean('completed').defaultTo(false);
+
+        //task foreign key here ??
+        tbl.integer('project_id'); // i think this will link to project
+      })
+      //connector table -- foreign keys to connect all tables together
+      .createTable('resource_task_project', tbl => {
+        tbl.increments();
+
+        tbl
+          .integer('task_id')
+          .references('id')
+          .inTable('tasks')
+          .onDelete('RESTRICT')
+          .onUpdate('CASCADE');
+        tbl
+          .integer('resource_id')
+          .references('id')
+          .inTable('resources')
+          .onDelete('RESTRICT')
+          .onUpdate('CASCADE');
+        tbl
+          .integer('project_id')
+          .references('id')
+          .inTable('projects')
+          .onDelete('RESTRICT')
+          .onUpdate('CASCADE');
+      })
+  );
 };
 
-exports.down = function(knex) {};
+exports.down = function(knex) {
+  //add in reverse order as tables created
+  return knex.schema
+    .dropTableIfExists('resource_task_project')
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('projects')
+    .dropTableIfExists('resources');
+};
