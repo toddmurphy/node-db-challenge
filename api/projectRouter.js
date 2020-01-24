bf06878d61714a95b7dfdc0b74570bea5db9c296
toddmurphy <toddmurphy93@gmail.com>
2020-01-24 17:45:20 -0400

    working on strech crud for projects

--- api/projectRouter.js
+++ api/projectRouter.js
@@ -15,6 +15,40 @@ router.get('/', (req, res) => {
     });
 });
 
+//get --> single project by 'id'
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  Projects.getProjectByID(id)
+    .then(project => {
+      if (project) {
+        res.status(200).json(project);
+      } else {
+        res.status(401).json({ message: 'Sorry, that project id not returned' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'Sorry, single project by that id not returned from server', error });
+    });
+});
+
+//delete --> remove a single project by 'id'
+router.delete('/:id', (req, res) => {
+  const deletedID = req.params;
+
+  Projects.deleteProject(deletedID)
+    .then(deletedAction => {
+      if (deletedAction) {
+        res.status(200).json(deletedAction);
+      } else {
+        res.status(401).json({ message: 'Sorry, project not deleted' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'Sorry, project not deleted from server', error });
+    });
+});
+
 //post --> create new project
 router.post('/', (req, res) => {
   const projectData = req.body;
@@ -25,6 +59,20 @@ router.post('/', (req, res) => {
     })
     .catch(error => {
       res.status(500).json({ message: 'Sorry, unable to create new project on server', error });
+    });
+});
+
+//put --> update a post by 'id'
+router.put('/:id', (req, res) => {
+  const updatedID = req.params.id;
+  const updatedData = req.body;
+
+  Projects.updateProject(updatedID, updatedData)
+    .then(updatedInfo => {
+      res.status(200).json(updatedInfo);
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'Sorry, project with that id not updated on server', error });
     });
 });
 
--- projects/project-model.js
+++ projects/project-model.js
@@ -3,7 +3,10 @@ const db = require('../data/dbConfig');
 module.exports = {
   //project helper functions to export
   getProjects,
-  addProject
+  addProject,
+  getProjectByID,
+  deleteProject,
+  updateProject
 };
 
 //get all projects
@@ -11,7 +14,31 @@ function getProjects() {
   return db('projects');
 }
 
+//get single project by 'id'
+function getProjectByID(id) {
+  return db('projects').where('id', id);
+}
+
+//delete single project by 'id'
+function deleteProject(id) {
+  return db('projects')
+    .where('id', id)
+    .del();
+}
+
 //add single project
 function addProject(project) {
   return db('projects').insert(project);
+}
+
+//update single project by 'id
+function updateProject(updatedInfo, id) {
+  return db('projects')
+    .where('id', id)
+    .update(updatedInfo)
+    .then(() => {
+      db.update('projects')
+        .where('id', id)
+        .first();
+    });
 }
